28c486c94443cbfca5f37d07b0f790d35b5fe5cd
Cem Topuk <cemtopuk.ct@gmail.com>
2019-12-05 10:50:06 +0300

    Add files via upload

---
+++ Ödev1/Ödev1/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(256, 29)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 47)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(256, 46)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Crypte"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label1.Location = New System.Drawing.Point(8, 96)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 24)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Crypted Text:"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 24
+        Me.ListBox1.Location = New System.Drawing.Point(12, 123)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(256, 220)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(287, 363)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Crypter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Ödev1/Ödev1/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Function findPrime(ByVal number As Integer, ByVal direction As Integer) As String
+        Dim primeFlag As Boolean
+        Dim primeI As Integer
+
+        While True
+            number += direction
+            primeFlag = True
+            For primeI = 2 To number / 2
+                If number Mod primeI = 0 Then
+                    primeFlag = False
+                End If
+            Next
+            If primeFlag Then
+                Exit While
+            End If
+        End While
+        Return number.ToString()
+    End Function
+
+    Function processNum(ByVal strData As String, ByVal process As Boolean) As String
+        Dim sum As Integer
+        For Each ch As Char In strData
+            Dim i As Integer
+            If process And Integer.TryParse(ch, i) Then
+                sum += i
+            ElseIf Integer.TryParse(ch, i) Then
+                sum -= i
+                sum = Math.Abs(sum)
+            End If
+        Next
+        If sum < 10 And process Then
+            Return ("0" + sum.ToString())
+        Else
+            Return sum.ToString()
+        End If
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ListBox1.Items.Clear()
+
+        Dim charD As Char
+        Dim textData() As Char = TextBox1.Text.ToString()
+        Dim charToInt As Integer
+        Dim actualData(12) As Char
+
+        For i = 0 To textData.Length - 1
+            charD = textData(i)
+            charToInt = Asc(charD)
+            Dim lowPrime As String = findPrime(charToInt, -1)
+            Dim highPrime As String = findPrime(charToInt, 1)
+            ListBox1.Items.Add(processNum(charToInt.ToString(), True) + processNum(lowPrime, False) + processNum(lowPrime, True) + lowPrime + processNum(highPrime, False) + processNum(highPrime, True) + highPrime)
+        Next
+    End Sub
+End Class
